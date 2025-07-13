42fdcd1aec29cd9709f583171e829002e1fa9c9f
pHH <gudghks56@gmail.com>
2025-07-14 01:53:59 +0900

    Image util add

---
+++ TravelProject/Util/ImageGen.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+enum ImageGen {
+    static let checkFill = UIImage(systemName: "checkmark.square.fill")
+    static let check =     UIImage(systemName: "checkmark.square")
+    static let star  =     UIImage(systemName: "star")
+    static let starFill  = UIImage(systemName: "star.fill")
+    static let heart     = UIImage(systemName: "heart")
+    static let heartFill = UIImage(systemName: "heart.fill")
+    static let clockwise = UIImage(systemName: "arrow.clockwise")
+}
