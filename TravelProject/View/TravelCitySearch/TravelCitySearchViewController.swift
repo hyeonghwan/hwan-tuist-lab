6f9083a38a0d7f7a4bf5c9e511b34b8b588e4e6f
pHH <gudghks56@gmail.com>
2025-07-15 23:18:24 +0900

    design: TravelCitySearchVC 생성 + storyBoard 연결

---
+++ TravelProject/View/TravelCitySearch/TravelCitySearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TravelCitySearchViewController.swift
+//  TravelProject
+//
+//  Created by hwan on 7/15/25.
+//
+
+import UIKit
+
+final class TravelCitySearchViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
