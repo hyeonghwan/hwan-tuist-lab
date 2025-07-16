1185821c374ff6bd015b4041d640debab1d4cc78
pHH <gudghks56@gmail.com>
2025-07-16 09:56:06 +0900

    tuist: Tuist Folder Setting

---
+++ Tuist.swift
@@ -0,0 +1,6 @@
+import ProjectDescription
+
+
+let tuist = Tuist(
+    project: .tuist(plugins: [.git(url: "https://github.com/hyeonghwan/TuistPlugin.git", sha: "c6099ab8d65e3a7cb5173d78215e9e549ed09198")])
+)
