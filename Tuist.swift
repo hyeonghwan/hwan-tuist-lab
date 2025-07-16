61f169126a2a58f4c730b8d337d0a0abf2efc547
pHH <gudghks56@gmail.com>
2025-07-16 11:17:14 +0900

    tuist: Plugin 추가

--- Tuist.swift
+++ Tuist.swift
@@ -2,5 +2,5 @@ import ProjectDescription
 
 
 let tuist = Tuist(
-    project: .tuist(plugins: [.git(url: "https://github.com/hyeonghwan/TuistPlugin.git", sha: "c6099ab8d65e3a7cb5173d78215e9e549ed09198")])
+    project: .tuist(plugins: [.git(url: "https://github.com/hyeonghwan/TuistPlugin.git", sha: "f5fc398c99701dee83a444aa59b9cdce120f2c8d")])
 )
