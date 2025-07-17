b77ef2cb2fbab8a38529f288a0e06304f88e8819
pHH <gudghks56@gmail.com>
2025-07-17 16:42:17 +0900

    fix: ShadowView Background , ShadowLayer 재생성 방지
    - Cell 재사용시 Shadow Layer 다시 생성하는 로직에서 -> 한번만 생성 하도록 변경
    - super.draw 삭제 -> 상속관계 에서 parent, child가 draw 를 구현하는 경우에만 child 에서 호출 해야됨

--- Projects/Travel/Sources/View/TravelCitySearch/View/CityCell.swift
+++ Projects/Travel/Sources/View/TravelCitySearch/View/CityCell.swift
@@ -15,41 +15,26 @@ final class CityCell: UITableViewCell, CellIdentifialble {
     @IBOutlet weak var shadowView: UIView!
     
     private var roundedLayer: CAShapeLayer! = nil
-    private var shadowLayer: CAShapeLayer? = nil
+    private var shadowLayer: CAShapeLayer! = nil
     private var setLayer: Bool = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
         containerView.clipsToBounds = true
         containerView.layer.masksToBounds = false
-        containerView.backgroundColor = .clear
+        containerView.backgroundColor = .systemBackground
         self.cityImageView.clipsToBounds = true
         shadowView.backgroundColor = .clear
         self.sendSubviewToBack(shadowView)
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        if setLayer == true && shadowLayer == nil {
-            shadowLayer = CAShapeLayer()
-            shadowLayer?.name = "Shadow"
-            makeShadowLayer(layer: shadowLayer!)
-            shadowView.layer.insertSublayer(shadowLayer!, at: 0)
-        }
-    }
-    
-    private func makeShadowLayer(layer: CAShapeLayer) {
-        layer.path = UIBezierPath(roundedRect: shadowView.bounds, cornerRadius: 25).cgPath
-        layer.fillColor = UIColor.clear.cgColor
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowPath = layer.path
-        layer.shadowOffset = CGSize(width: 5, height: 5)
-        layer.shadowOpacity = 0.2
-        layer.shadowRadius = 3
-    }
-
     override func draw(_ rect: CGRect) {
-        super.draw(rect)
+        if shadowLayer == nil {
+            shadowLayer = CAShapeLayer()
+            makeShadowLayer(layer: shadowLayer)
+            shadowView.layer.insertSublayer(shadowLayer, at: 0)
+        }
+        
         if roundedLayer == nil {
             roundedLayer = CAShapeLayer()
             let path = UIBezierPath(
@@ -62,14 +47,20 @@ final class CityCell: UITableViewCell, CellIdentifialble {
         }
     }
     
+    private func makeShadowLayer(layer: CAShapeLayer) {
+        layer.name = "Shadow"
+        layer.path = UIBezierPath(roundedRect: shadowView.bounds, cornerRadius: 25).cgPath
+        layer.fillColor = UIColor.clear.cgColor
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowPath = layer.path
+        layer.shadowOffset = CGSize(width: 5, height: 5)
+        layer.shadowOpacity = 0.2
+        layer.shadowRadius = 3
+    }
+    
     override func prepareForReuse() {
         super.prepareForReuse()
-        shadowLayer = nil
         cityImageView.kf.cancelDownloadTask()
-        setLayer = false
-        for sublayer in shadowView.layer.sublayers ?? [] where sublayer.name == "Shadow" {
-            sublayer.removeFromSuperlayer()
-        }
     }
     
     func set(info: City, contains: String?) {
