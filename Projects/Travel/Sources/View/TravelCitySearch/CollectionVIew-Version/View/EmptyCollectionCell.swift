f23443d2212facc126c8ab28663b02fcacf8e4fe
pHH <gudghks56@gmail.com>
2025-07-18 10:01:41 +0900

    feat: EmptyCollectionCell + xib

--- Projects/Travel/Sources/View/TravelCitySearch/CollectionVIew-Version/View/EmptyCollectionCell.swift
+++ Projects/Travel/Sources/View/TravelCitySearch/CollectionVIew-Version/View/EmptyCollectionCell.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 
-final class EmptyCollectionCell: UICollectionViewCell {
+final class EmptyCollectionCell: UICollectionViewCell, CellIdentifialble {
+    
+    private let label = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        label.textColor = .secondaryLabel
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 21, weight: .ultraLight)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        contentView.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30),
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    func set(keyword: String) {
+        label.text = "\"\(keyword)\"에 해당하는 결과를 찾을 수 없습니다."
     }
 }
 
