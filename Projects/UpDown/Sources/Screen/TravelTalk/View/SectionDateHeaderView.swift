ce68c19ac0b6280c48b3c5c41366fedf0e7dea43
pHH <gudghks56@gmail.com>
2025-07-21 16:13:11 +0900

    design/talk: SectionDateHeader add
    - CollectionView section 을 date header로 나누기 위한 Component

--- Projects/UpDown/Sources/Screen/TravelTalk/View/SectionDateHeaderView.swift
+++ Projects/UpDown/Sources/Screen/TravelTalk/View/SectionDateHeaderView.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2025 com.hwan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class SectionDateHeaderView: UICollectionReusableView, CellIdentifialble {
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    private func addLayout() {
+        self.addSubview(dateLabel)
+        NSLayoutConstraint.activate([
+            dateLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            dateLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+    
+    func configure(with date: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateLabel.text = dateFormatter.string(from: date)
+    }
+}
