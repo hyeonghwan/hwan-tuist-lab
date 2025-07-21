23b02437dee451032a52a4544ac34bdff54202a7
pHH <gudghks56@gmail.com>
2025-07-21 09:51:12 +0900

    design/talk: TravelTalkCell + Xib 추가

--- Projects/UpDown/Sources/Screen/TravelTalk/View/TravelTalkCell.swift
+++ Projects/UpDown/Sources/Screen/TravelTalk/View/TravelTalkCell.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2025 com.hwan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
+
+final class TravelTalkCell: UICollectionViewCell, CellIdentifialble {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nickNameLabel: UILabel!
+    @IBOutlet weak var conversationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        profileImage.layer.cornerRadius = 25
+        profileImage.layer.borderWidth = 0.1
+        profileImage.layer.borderColor = UIColor.secondaryLabel.cgColor
+    }
+    
+    func configure(info room: ChatRoom) {
+        profileImage.image = UIImage(named: "\(room.chatroomImage)") ?? ImageGen.star
+        nickNameLabel.text = room.chatroomName
+        
+        conversationLabel.text
+        = if let chat = room.chatList.last {
+            chat.message
+        } else {
+            ""
+        }
+        
+        dateLabel.text
+        = if let chat = room.chatList.last {
+            chat.date
+                .toDate("yyyy-MM-dd HH:mm")?
+                .toFormat("yy.MM.dd")
+        } else {
+            ""
+        }
+    }
+}
