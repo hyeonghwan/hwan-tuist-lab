7437012cfe34116132f9572fff9f7e8960d36770
pHH <gudghks56@gmail.com>
2025-07-21 09:49:44 +0900

    feat: Image nameSpace

--- Projects/UpDown/Sources/Screen/TravelTalk/Util/ImageGen.swift
+++ Projects/UpDown/Sources/Screen/TravelTalk/Util/ImageGen.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2025 com.hwan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageGen {
+    static let checkFill = UIImage(systemName: "checkmark.square.fill")
+    static let check =     UIImage(systemName: "checkmark.square")
+    static let star  =     UIImage(systemName: "star")
+    static let starFill  = UIImage(systemName: "star.fill")
+    static let heart     = UIImage(systemName: "heart")
+    static let heartFill = UIImage(systemName: "heart.fill")
+    static let clockwise = UIImage(systemName: "arrow.clockwise")
+    static let paper_plane = UIImage(named: "paper_plane")
+}
+
