c52cf8bb359518df1560dae018c0ed2176ffd690
pHH <gudghks56@gmail.com>
2025-07-21 09:49:35 +0900

    feat: DateFormatter extension

--- Projects/UpDown/Sources/Screen/TravelTalk/Util/DateFormatter.swift
+++ Projects/UpDown/Sources/Screen/TravelTalk/Util/DateFormatter.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+
+fileprivate enum DateResolver {
+    static let formatter = DateFormatter()
+}
+
+extension Date {
+    func toFormat(_ format: String = "yyyy년 MM월 dd일") -> String {
+        let formatter = DateResolver.formatter
+        formatter.locale = Locale(identifier:"ko_KR")
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+}
+
+extension String {
+    func toFormatted() -> Self {
+        self.toDate()?.toFormat() ?? Date.now.toFormat()
+    }
+    
+    func toDate(_ format: String = "yyMMdd") -> Date? {
+        let formatter = DateResolver.formatter
+        formatter.dateFormat = format
+        formatter.locale = Locale(identifier:"ko_KR")
+        return formatter.date(from: self)
+    }
+}
+
