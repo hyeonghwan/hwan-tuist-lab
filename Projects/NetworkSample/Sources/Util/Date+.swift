efdbcb94ab1b4dcf1ec82b94d12a53ce376c8fbd
pHH <gudghks56@gmail.com>
2025-07-25 07:51:54 +0900

    feat/network: add Util String Date

--- Projects/NetworkSample/Sources/Util/Date+.swift
+++ Projects/NetworkSample/Sources/Util/Date+.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+extension Date {
+    static var yesterday: Date { return Date.now.dayBefore }
+    static var tomorrow:  Date { return Date.now.dayAfter }
+    
+    var dayBefore: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    
+    var dayAfter: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    
+    var month: Int {
+        return Calendar.current.component(.month,  from: self)
+    }
+    
+    var isLastDayOfMonth: Bool {
+        return dayAfter.month != month
+    }
+}
