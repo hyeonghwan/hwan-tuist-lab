ac034264230ca21eaa2d845cf5317fc86997b480
pHH <gudghks56@gmail.com>
2025-07-25 07:53:51 +0900

    feat/network: API Resource Type add

---
+++ Projects/NetworkSample/Sources/Services/API/APIResource.swift
@@ -0,0 +1,68 @@
+//
+//  API.swift
+//  NetworkSample
+//
+//  Created by hwan on 7/24/25.
+//  Copyright © 2025 com.hwan. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+protocol APIResource {
+    associatedtype ResponseType: Decodable
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var API_KEY: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+    var body: Data? { get }
+}
+
+extension APIResource {
+    var scheme: String {
+        "https"
+    }
+    var host: String {
+        "api.example.com"
+    }
+    var headers: [String: String]? {
+        ["Content-Type": "application/json"]
+    }
+    var body: Data? {
+        nil
+    }
+    
+    func urlRequest(queries: [String: String?]) throws -> URLRequest {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.path = path
+        
+        var queries = queries
+        if !self.API_KEY.isEmpty {
+            queries["key"] = self.API_KEY
+        }
+        components.queryItems = queries.reduce(into: [URLQueryItem]()) { origin, next in
+            origin.append(URLQueryItem(name: next.key, value: next.value))
+        }
+        
+        guard let url = components.url else {
+            throw URLError(.badURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = headers
+        request.httpBody = body
+        
+        return request
+    }
+}
