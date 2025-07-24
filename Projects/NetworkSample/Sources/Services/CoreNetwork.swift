7d85bb60c32165be85615a5ca2fe07d41de57b7b
pHH <gudghks56@gmail.com>
2025-07-25 07:54:08 +0900

    feat/network: Core Network 추가

--- Projects/NetworkSample/Sources/Services/CoreNetwork.swift
+++ Projects/NetworkSample/Sources/Services/CoreNetwork.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import Alamofire
+import Combine
+
+// @Logging
+final class CoreNetwork {
+    private let session = URLSession(configuration: .default)
+    static let shared = CoreNetwork()
+    
+    private init() {}
+    
+    func get<Resource, DTO>(resource: Resource, type: DTO.Type, queries: [String: String], completion: @escaping (Result<DTO, Error>) -> Void) where Resource: APIResource, DTO: Codable {
+        do {
+            let urlRequest = try resource.urlRequest(queries: queries)
+            AF.request(urlRequest, interceptor: .retryPolicy)
+                .responseDecodable(of: DTO.self) { result in
+                    switch result.result {
+                    case let .success(dto):
+                        completion(.success(dto))
+                        
+                    case let .failure(error):
+                        completion(.failure(error))
+                    }
+                }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    func get(destination url: URL) {
+        
+    }
+    
+    func put() {
+        
+    }
+    
+    func patch() {
+        
+    }
+    
+    func delete() {
+        
+    }
+}
