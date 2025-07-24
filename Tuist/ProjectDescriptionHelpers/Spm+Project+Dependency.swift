5cc0beb7ca2bc086a38931bc8cf459d8dea842bb
pHH <gudghks56@gmail.com>
2025-07-25 07:54:34 +0900

    build/network: Alamofire dependency add

--- Tuist/ProjectDescriptionHelpers/Spm+Project+Dependency.swift
+++ Tuist/ProjectDescriptionHelpers/Spm+Project+Dependency.swift
@@ -10,12 +10,14 @@ public extension TargetDependency.SPM {
     static let kingfisher: TargetDependency = .external(name: "Kingfisher")
     static let hwanKit: TargetDependency = .external(name: "HwanKit")
     static let hwanMacros: TargetDependency = .external(name: "HwanMacros")
+    static let alamofire: TargetDependency = .external(name: "Alamofire")
 }
 
 public enum R {
     public static let kingfisher: Package = .package(url: "https://github.com/onevcat/Kingfisher", .upToNextMajor(from: Version(8, 4, 0)))
     public static let hwanKit: Package = .package(url: "https://github.com/hyeonghwan/hwan-kit", .revision("d89cc1648a007c5ae9077ae7e5cbbeccff0a8675"))
     public static let hwanMacros: Package = .package(url: "https://github.com/hyeonghwan/hwan_macro", .revision("270ac9b6c28c552e9d02ccf9ec6bf204d77633f4"))
+    public static let alamofire: Package = .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: Version(5, 10, 0)))
 }
 
 public enum P {
