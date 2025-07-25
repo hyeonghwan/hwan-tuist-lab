727e660d606f8ddcf1d59cfcfa600a49ac6cd9fd
pHH <gudghks56@gmail.com>
2025-07-25 19:25:00 +0900

    feat/network: NaverAPI error type

--- Projects/NetworkSample/Sources/Services/Error/NaverApiError.swift
+++ Projects/NetworkSample/Sources/Services/Error/NaverApiError.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+enum APIError: Error {
+    case decodingError(String)
+}
+
+struct NaverSearchAPIError: Decodable {
+    let errorMessage: String
+    let errorCode: NaverApiError
+}
+
+enum NaverApiError: String, Error, Decodable {
+    case INCORRECT_QUERY = "SE01"
+    case INVALID_DISPLAY_VALUE = "SE02"
+    case INVALID_START_VALUE = "SE03"
+    case INVALID_SORT_VALUE = "SE04"
+    case MALFORMED_ENCODING = "SE06"
+    case INVALID_SEARCH_API = "SE05"
+    case SYSTEM_ERROR = "SE99"
+    
+    var message: String {
+        switch self {
+        case .INCORRECT_QUERY:
+            return "잘못된 쿼리요청입니다."
+        case .INVALID_DISPLAY_VALUE:
+            return "부적절한 display 값입니다."
+        case .INVALID_START_VALUE:
+            return "부적절한 start 값입니다."
+        case .INVALID_SORT_VALUE:
+            return "부적절한 sort 값입니다."
+        case .MALFORMED_ENCODING:
+            return "잘못된 형식의 인코딩입니다."
+        case .INVALID_SEARCH_API:
+            return "존재하지 않는 검색 api 입니다."
+        case .SYSTEM_ERROR:
+            return "시스템 에러"
+        }
+    }
+}
