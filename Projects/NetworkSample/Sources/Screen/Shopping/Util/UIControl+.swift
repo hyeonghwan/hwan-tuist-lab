2d07737ed5842480be3e4614f7b1dac573fa9203
pHH <gudghks56@gmail.com>
2025-07-27 22:48:59 +0900

    refactor/naver:  UIControl extension
    - Control component observe 하기 위함

--- Projects/NetworkSample/Sources/Screen/Shopping/Util/UIControl+.swift
+++ Projects/NetworkSample/Sources/Screen/Shopping/Util/UIControl+.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+extension UIControl {
+    typealias _ControlPublisher = UIControl.ControlPublisher
+    typealias _Event = UIControl.Event
+    
+    func controlPublisher(for event: Event) -> ControlPublisher {
+        return ControlPublisher(control: self, event: event)
+      }
+    
+    struct ControlPublisher: Publisher {
+        typealias Output = UIControl
+        typealias Failure = Never
+        
+        let control: UIControl
+        let event: UIControl.Event
+        
+        func receive<T>(subscriber: T) where T: Subscriber, Never == T.Failure, UIControl == T.Input {
+            let subscription = EventSubscription(control: control, subscrier: subscriber, event: event)
+            subscriber.receive(subscription: subscription)
+        }
+    }
+    
+    private class EventSubscription<EventSubscriber: Subscriber>: Subscription where EventSubscriber.Input == UIControl, EventSubscriber.Failure == Never {
+        let control: UIControl
+        let event: UIControl.Event
+        var subscriber: EventSubscriber?
+        
+        init(control: UIControl, subscrier: EventSubscriber, event: UIControl.Event) {
+            self.control = control
+            self.subscriber = subscrier
+            self.event = event
+            control.addTarget(self, action: #selector(eventPublished), for: event)
+        }
+        
+        func request(_ demand: Subscribers.Demand) {}
+        
+        func cancel() {
+            subscriber = nil
+            control.removeTarget(self, action: #selector(eventPublished), for: event)
+        }
+        
+        @objc func eventPublished() {
+            _ = subscriber?.receive(control)
+        }
+    }
+}
