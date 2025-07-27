35fb6096b96202182c3a1d6d26411395574817bb
pHH <gudghks56@gmail.com>
2025-07-27 23:06:30 +0900

    feat/refactor/naver: Shopping CollectionView Paging Controller Object refactor
    - Pagenation Controller 객체 분리
    - 스크롤 뷰의 화면 비율 75% 이상 일시 콘텐츠 pagenation subject 호출 하도록 구성

--- Projects/NetworkSample/Sources/Screen/Shopping/ViewController/ShoppingResultViewController.swift
+++ Projects/NetworkSample/Sources/Screen/Shopping/ViewController/ShoppingResultViewController.swift
@@ -18,7 +18,11 @@ final class ShoppingResultViewController: BaseViewController {
     private let refreshControl = UIRefreshControl()
     
     private let scrollAnimator = ScrollAnimator()
-    
+    private lazy var pagenationController = PagenationController(
+        scrollView: collectionView,
+        pagingSubject: shoppingPagingSubject
+    )
+    private let shoppingPagingSubject = PassthroughSubject<Void, Never>()
     
     override func addChild() {
         self.view.addSubview(headerView)
@@ -89,6 +93,75 @@ extension ShoppingResultViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         CGSize(width: windowWidth, height: 100)
+    }
+}
+
+
+// MARK: PagenationController
+extension ShoppingResultViewController {
+    @Logging
+    fileprivate class PagenationController {
+        private weak var scrollView: UIScrollView!
+        private var pagingSubject: PassthroughSubject<(Void), Never>
+        private var cancellable: AnyCancellable?
+        
+        init(scrollView: UIScrollView, pagingSubject: PassthroughSubject<Void, Never>) {
+            self.pagingSubject = pagingSubject
+            self.scrollView = scrollView
+        }
+        
+        func observe(_ isApiLoading: AnyPublisher<Bool, Never>) {
+            guard let scrollView = self.scrollView else { return }
+            cancellable = scrollView.publisher(for: \.contentOffset)
+                .removeDuplicates(by: { $0.y == $1.y })
+                .throttle(for: .milliseconds(600), scheduler: DispatchQueue.main, latest: true)
+                .combineLatest(isApiLoading)
+                .filter { (_, isLoading) in
+                    return !isLoading
+                }
+                .filter { [weak self] _ in
+                    (self?.scrollView?.contentSize.height ?? 0) > 0
+                }
+                .sinkWeak(on: self) { (controller, _) in
+                    guard let scrollView = controller.scrollView else { return }
+                    let offsetY = scrollView.contentOffset.y
+                    let contentHeight = scrollView.contentSize.height
+                    let boundsHeight = scrollView.bounds.height
+                    let totalOffset = offsetY + boundsHeight
+                    if totalOffset > (contentHeight * 3) / 4 {
+                        self.pagingSubject.send()
+                    }
+                }
+        }
+        
+        private func logging(pointY: CGFloat) {
+            // let logString = """
+            //  \(#function) -
+            //  offsetY: \(offsetY)
+            //  request to viewmodel totalOFfset: \(totalOffset)
+            //  contentHeight: \(contentHeight - 200)
+            //  isRequest: \(totalOffset > (contentHeight * 3) / 4),
+            //  viewModel.isApiLoadingSubject: \(String(describing: self.viewModel.isApiLoadingSubject.value))
+            //  """
+            // self.logger.log(
+            //     level: .info,
+            //     "\(logString)"
+            // )
+            let contentSize = self.scrollView?.contentSize ?? CGSize(width: 0, height: 0)
+            let boundsHeight = self.scrollView?.bounds.height ?? 0
+            let log = """
+            \(Self.self) ----------------------------------------------------- 
+            contentOffsetY: \(String(describing: pointY))
+            bounds.height:  \(String(describing: boundsHeight))
+            offsetY + bound.height \(pointY + boundsHeight)
+            contentSize:    \(String(describing: contentSize))
+            ------------------------------------------------------------------
+            """
+            self.logger.log(
+                level: .info,
+                "\(log)"
+            )
+        }
     }
 }
 extension ShoppingResultViewController {
