c1496558a276187096ca6d04a6ae1102be5b9c71
pHH <gudghks56@gmail.com>
2025-07-27 23:20:21 +0900

    feat/naver: ShoppingProvider - Naver Search API 호출 인스턴스

--- Projects/NetworkSample/Sources/Screen/Shopping/Provider/ShoppingProvider.swift
+++ Projects/NetworkSample/Sources/Screen/Shopping/Provider/ShoppingProvider.swift
@@ -7,3 +7,101 @@
 //
 
 import Foundation
+import Combine
+
+struct ShoppingProvider {
+    var fetchWithClosure: (_ query: ShoppingSearchQuery,
+                           _ completion: @escaping (Result<ShoppingItemResultDTO, any Error>) -> Void) -> Void
+    
+    var fetchWithTask: (_ query: ShoppingSearchQuery) async -> Result<ShoppingItemResultDTO, any Error>
+    
+    var fetchWithPublisher: (_ query: ShoppingSearchQuery) -> AnyPublisher<ShoppingItemResultDTO, any Error>
+}
+
+extension ShoppingProvider {
+    static func makeResource(_ start: Int, _ display: Int, _ query: String, _ sort: ShoppingSortType) -> NaverShoppingSearchApiResource {
+        NaverShoppingSearchApiResource(
+            query: ShoppingSearchQuery(
+                query: query,
+                display: display,
+                start: start,
+                sort: sort.string
+            )
+        )
+    }
+    
+    static func makeResource(query: ShoppingSearchQuery) -> NaverShoppingSearchApiResource {
+        NaverShoppingSearchApiResource(
+            query: query
+        )
+    }
+}
+
+extension ShoppingProvider {
+    static let liveValue = ShoppingProvider(
+        fetchWithClosure: { query, completion in
+            CoreNetwork.shared.GET(
+                resource: makeResource(query: query),
+                decodeType: ShoppingItemResultDTO.self,
+                completion: completion
+            )
+        },
+        fetchWithTask: { query in
+            await CoreNetwork.shared.GET(
+                resource: makeResource(query: query),
+                decodeType: ShoppingItemResultDTO.self
+            )
+        },
+        fetchWithPublisher: { query in
+            CoreNetwork.shared.GET(
+                resource: makeResource(query: query),
+                decodeType: ShoppingItemResultDTO.self
+            )
+        }
+    )
+    
+    static let testValue = ShoppingProvider(
+        fetchWithClosure: { query, completion  in
+            
+        },
+        fetchWithTask: { query in
+            await Task.detached {
+                return Result<ShoppingItemResultDTO, any Error>.init(
+                    catching: {
+                        ShoppingItemResultDTO(
+                            data: NaverSearchResultDTO(
+                                lastBuildDate: "sdfsdfsf",
+                                total: 1,
+                                start: 0,
+                                display: 1,
+                                items: []
+                            )
+                        )
+                    }
+                )
+            }.value
+        },
+        fetchWithPublisher: { query in
+            Deferred {
+                Future<ShoppingItemResultDTO, any Error> { promise in
+                    promise(
+                        Result<ShoppingItemResultDTO, any Error>.init(
+                            catching: {
+                                ShoppingItemResultDTO(
+                                    data: NaverSearchResultDTO(
+                                        lastBuildDate: "sdfsdfsf",
+                                        total: 1,
+                                        start: 0,
+                                        display: 1,
+                                        items: []
+                                    )
+                                )
+                            }
+                        )
+                    )
+                }
+            }
+            .eraseToAnyPublisher()
+        }
+    )
+}
