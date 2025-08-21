e9b7938ccf549ec622ab60b6c8c2ccc3bcbe8d8f
pHH <gudghks56@gmail.com>
2025-08-21 19:14:57 +0900

    feat(photo): update App event logger and scene delegate

--- Projects/PhotoFeature/Sources/App/SceneDelegate.swift
+++ Projects/PhotoFeature/Sources/App/SceneDelegate.swift
@@ -1,41 +1,77 @@
 import UIKit
+import APIClient
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    let sessionRegistry = AFSessionRegistry(monitor: APIEventLogger())
+    lazy var defaultAPIClient = DefaultAPIClient(registry: sessionRegistry)
+    let favoriteStore = FavoriteStore(cache: UserFavoriteCache.shared)
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow()
+        window?.windowScene = windowScene
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        let searchViewController = SearchViewController.create(
+            with: SearchViewModel(
+                favoriteStore: favoriteStore,
+                provider: SearchProvider(
+                    defaultAPIClient
+                )
+            )
+        )
+        
+        let topicViewController = TopicViewController
+            .create(
+                with: TopicViewModel(
+                    topicProvider: TopicProvider(defaultAPIClient)
+                )
+            )
+        
+        topicViewController.navigationItem.title = "OUR TOPIC"
+        
+        let nav1 = UINavigationController(rootViewController: topicViewController)
+        nav1.navigationBar.prefersLargeTitles = false
+        nav1.tabBarItem = UITabBarItem.init(
+            title: nil,
+            image: UIImage(systemName: "chart.line.uptrend.xyaxis"),
+            tag: 0
+        )
+           
+        let dummyOne = DummyViewController()
+        dummyOne.tabBarItem = UITabBarItem.init(
+            title: nil,
+            image: UIImage(systemName: "play.rectangle"),
+            tag: 1
+        )
+        
+        let nav = UINavigationController(rootViewController: searchViewController)
+        nav.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "magnifyingglass"),
+            tag: 2
+        )
+        
+        let dummyTwo = DummyViewController()
+        dummyTwo.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "heart"),
+            tag: 3
+        )
+        
+        let tabBarController = UITabBarController()
+        
+        tabBarController.setViewControllers([nav1, dummyOne , nav, dummyTwo], animated: false)
+        tabBarController.tabBar.tintColor = .black
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
     }
+}
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+final class DummyViewController: UIViewController {
+    override func viewDidLoad() {
+        self.view.backgroundColor = .white
     }
 }
